04a1afe6ffd27a0e489993181bd6d5e97d639da2
Amin <aminzabardast@gmail.com>
2020-12-22 13:04:38 +0300

    Making data recovery steps more robust

--- download_data.sh
+++ download_data.sh
@@ -3,10 +3,18 @@
 directory=~/.DeepSpliceDetectors
 mkdir -p "$directory"
 
+project=$(pwd);
+md5="$directory/data.md5"
+cp ./data.md5 "$md5"
+cd "$directory" || exit
+
 output="$directory/data.tar.gz"
-if ! [ -f $output ]; then
+if ! md5sum --quiet --status -c "$md5"; then
   curl -sL -o $output "https://drive.google.com/uc?export=download&id=1RY2n3sSciln3qIEVVFs-f5_lG-EXn9q1"
 fi
 
-tar -xf $output -C $directory
-echo "Data is downloaded and extracted!"
+if tar -xf $output -C $directory; then
+  echo "Data is downloaded and extracted!"
+else
+  echo "Something went wrong! try again."
+fi
--- download_our_logs.sh
+++ download_our_logs.sh
@@ -4,17 +4,29 @@ while true; do
     read -p "Warning! This code might override the data in logs directory. Continue? (y/N)? " yn
     case $yn in
         [Yy]* ) break;;
-        * ) echo "Aborted!."; exit;;
+        * ) echo "Aborted!"; exit;;
     esac
 done
+
+project=$(pwd);
 
 directory=~/.DeepSpliceDetectors
 mkdir -p "$directory"
 
+md5="$directory/logs.md5"
+cp ./logs.md5 "$directory/logs.md5"
+
+cd "$directory" || exit
+
 output="$directory/logs.tar.gz"
-if ! [ -f $output ]; then
+if ! md5sum --quiet --status -c "$md5"; then
   curl -sL -o $output "https://drive.google.com/uc?export=download&id=1aPcXoVQEezFP_SsvEkX9XiDxhPXI5vmO"
 fi
 
-tar -xf $output -C ./
-echo "Done!"
+cd "$project" || exit
+
+if tar -xf $output -C ./; then
+  echo "Done!"
+else
+  echo "Something went wrong! Try again."
+fi
