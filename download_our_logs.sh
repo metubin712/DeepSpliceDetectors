e1e79896ee233363eb81a85881ce57d1ecf3a220
aminzabardast <RAZGRIZ!!stfighterjets4>
2021-04-10 22:07:29 +0300

    Refs #94 Updating logs and their checksums

--- download_our_logs.sh
+++ download_our_logs.sh
@@ -4,7 +4,7 @@ while true; do
     read -p "Warning! This code might override the data in logs directory. Continue? (y/N)? " yn
     case $yn in
         [Yy]* ) break;;
-        * ) echo "Aborted!"; exit;;
+        * ) echo "Nothing to do!"; exit;;
     esac
 done
 
@@ -18,14 +18,18 @@ cp ./logs.sha256 "$directory"
 
 cd "$directory" || exit
 
-output="$directory/logs.tar.gz"
+logs="$directory/logs.tar.gz"
 if ! shasum --quiet --status -a 256 -c "$sha"; then
-  curl -sL -o $output "https://drive.google.com/uc?export=download&id=1aPcXoVQEezFP_SsvEkX9XiDxhPXI5vmO"
+  curl -sL -o "$logs.aa" "https://drive.google.com/uc?export=download&id=1aKYip4pDWI9S0hIhKwhgCLmDqZ-wpZBf"
+  curl -sL -o "$logs.ab" "https://drive.google.com/uc?export=download&id=1SX0tU6g_YaiUSr2Vg-mpZPKYlUKVDAoW"
+  curl -sL -o "$logs.ac" "https://drive.google.com/uc?export=download&id=1_EvwAzF5A5LBH3bY1CIAhQb_UCXqbf6H"
 fi
 
 cd "$project" || exit
 
-if tar -xf $output -C ./; then
+cat "$logs.aa" "$logs.ab" "$logs.ac" > $logs
+
+if tar -xf $logs -C ./; then
   echo "Done!"
 else
   echo "Something went wrong! Try again."
